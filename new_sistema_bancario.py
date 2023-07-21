2e7c0c128a3a8230adb8ceb0c933618fbe55dc41
Thales Medeiros <thalesmedeiros@Thaless-MacBook-Air.local>
2023-07-21 17:52:26 -0300

    Update Funções

--- new_sistema_bancario.py
+++ new_sistema_bancario.py
@@ -22,35 +22,38 @@ def cad_conta_bancaria():
     return
 
 def depositar(valord, sal, ext):
-            if valord > 0:
-                sal += valord
-                global saldo
+    if valord > 0:
+        sal += valord
+        global saldo
+        saldo = sal
+        ext += f"Depósito: R$ {valord:.2f}\n"
+        global extrato
+        extrato = ext
+    else:
+        return "***Operação falhou! O valor informado é inválido.***"
+
+def sacar(valors, sal, ext):    
+
+    if valors > 0:
+        
+        if sal > valors:
+            
+            if valors <= val_max_saque:
+                
+                sal -= valors
+                global saldo 
                 saldo = sal
-                ext += f"Depósito: R$ {valord:.2f}\n"
+                ext += f"Saque: R$ {valors:.2f}\n"
                 global extrato
                 extrato = ext
+
             else:
-                return "***Operação falhou! O valor informado é inválido.***"
-
-def sacar(valors, sal, ext):
-                if valors > 0:
-                
-                    if valors <= val_max_saque:
-                    
-                        if sal > valors:
-                            sal -= valors
-                            global saldo 
-                            saldo = sal
-                            ext += f"Saque: R$ {valors:.2f}\n"
-                            global extrato
-                            extrato = ext
-
-                        else:
-                            return "***Operação falhou! Você não tem saldo suficiente.***"
-                    else:
-                        return f"***Operação falhou! O limite de saque é de R$ {val_max_saque}***"
-                else:
-                     return "***Operação falhou! O valor informado é inválido.***"
+                return f"***Operação falhou! O limite de saque é de R$ {val_max_saque:.2f}***"
+        else:
+            return "***Operação falhou! Você não tem saldo suficiente.***"
+    else:
+            return "***Operação falhou! O valor informado é inválido.***"
+                        
 
 def f_extrato(ext):
             global extrato
@@ -76,8 +79,8 @@ while True:
             
     elif opcao == "s":
         
-        if num_saque <= lim_saque:
-            num_saque +=1
+        if lim_saque > num_saque:
+            lim_saque -=1
             valor = float(input("Informe o valor do saque: "))
             
             print(sacar(valor, sal=saldo, ext=extrato))
